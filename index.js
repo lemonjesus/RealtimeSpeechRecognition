fb70d3cc8d1e2ca6624360e7ff92d32aa251c19d
Tucker Osman <osmiumusa@gmail.com>
2017-10-30 01:49:57 -0400

    Outputs the correct translations when it's finalized.

--- index.js
+++ index.js
@@ -14,21 +14,29 @@ const mic = lpcm16.start({
   threshold: 0,
   verbose: false,
   silence: '10.0',
-  device: 'plughw:2,0'
+  device: 'plughw:1,0'
 });
 
 //Start the uploading (speech) stream:
 let upOptions = {
-  url: `https://www.google.com/speech-api/full-duplex/v1/up?key=${config.key}&pair=${pair}&output=pb&lang=en-US&app=chromium&interim&continuous`,
+  url: `https://www.google.com/speech-api/full-duplex/v1/up?key=${config.key}&pair=${pair}&output=json&lang=en-US&app=chromium&interim&continuous`,
   headers: {
     "Content-Type": "audio/l16; rate=16000",
     "Referer": "https://docs.google.com"
   }
 }
 mic.pipe(request.post(upOptions));
-//fs.createReadStream('test.wav').pipe(request.post(upOptions));
+
+//Create a JSON stream parser
+const parser = new require("stream").Writable({
+  write: function(chunk, encoding, next) {
+    let obj = JSON.parse(chunk);
+    //console.log(obj);
+    if(obj && obj.result && obj.result[0] && obj.result[0].alternative && obj.result[0].alternative[0] && obj.result[0].alternative[0].transcript && obj.result[0].final)
+      console.log(obj.result[0].alternative[0].transcript);
+    next();
+  }
+});
 
 //Start the downloading (text) stream:
-request.get(`https://www.google.com/speech-api/full-duplex/v1/down?key=${config.key}&pair=${pair}&output=pb`).pipe(process.stdout);
-
-
+request.get(`https://www.google.com/speech-api/full-duplex/v1/down?key=${config.key}&pair=${pair}&output=json`).pipe(parser);
